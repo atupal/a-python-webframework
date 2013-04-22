7e5430afbbd346fe0b4dace37790169ed8143613
atupal <kangleyu@hustunique.com>
2013-04-22 20:21:02 +0800

    app

--- tmp/application.py
+++ tmp/application.py
@@ -17,5 +17,5 @@ app = ObjectPublisher(Root())
 
 if __name__ == "__main__":
     from paste import httpserver
-    httpserver.serve(app, host = '127.0.0.1', port = '8080')
+    httpserver.serve(app, host = '127.0.9.1', port = '8087')
 
